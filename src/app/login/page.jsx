08b4ed730d2767dd322db381a6914ee90e5fe41c
Dariana Rengifo <darianarengifo@gmail.com>
2024-11-14 14:22:23 -0300

    [main] update (src/app/*): rename

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 
+import Aside from "./components/Aside/Aside";
+import Form from "./components/Form/Form";
+
 export const metadata = {
   title: "¡Bienvenido a Fetch!",
   description: "Log in to your Fetch account to learn more about our services."
 };
 export default function Login() {
   return (
-    <div>
-      <h1>Login!</h1>
-    </div>
+    <>
+      <Aside />
+      <Form />
+    </>
   );
 }
