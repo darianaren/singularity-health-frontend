b21cd90175a7451a29d07abf39216bfac0da08c9
Dariana Rengifo <darianarengifo@gmail.com>
2024-11-15 15:12:49 -0300

    [main] update (src/hooks/useForm): documentation

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -2,6 +2,27 @@ import { useState, useCallback } from "react";
 
 import { APPLY_MASK } from "@/utils/constants/inputMasks";
 
+/**
+ * Custom hook to manage forms with validations and masks.
+ *
+ * @param {Object} config - Hook configuration.
+ * @param {Object} [config.applyMask={}] - Defines masks for the form fields. Keys are field names, and values are mask types.
+ * @param {Object} [config.initialForm={}] - Initial state of the form. Keys are field names, and values are the initial values.
+ * @param {Object} [config.validateForm={}] - Validation rules for the fields. Keys are field names, and values are objects with the rules:
+ *   - `required` (boolean): Indicates whether the field is required.
+ *   - `pattern` (RegExp): Regular expression to validate the field.
+ *   - `customValidator` (Function): Custom function to validate the field. Should return `true` if the field is invalid.
+ *   - `message` (string): Error message to display if validation fails.
+ *
+ * @returns {Object} - Methods and states to manage the form.
+ *   @property {Object} form - Current state of the form fields.
+ *   @property {Object} errors - Current validation errors.
+ *   @property {Function} handleChange - Handles changes in a form field, applying masks if defined.
+ *   @property {Function} blurValidator - Validates a specific field when it loses focus (`blur`).
+ *   @property {Function} formValidator - Validates all fields in the form.
+ *   @property {Function} resetForm - Resets the form to its initial state.
+ *   @property {Function} resetErrors - Clears all current errors.
+ */
 const useForm = ({ applyMask = {}, initialForm = {}, validateForm = {} }) => {
   const [form, setForm] = useState(initialForm);
   const [errors, setErrors] = useState({});
