2ab51ec8c8dc392bb29050fd666960e0d10f46f6
Dariana Rengifo <darianarengifo@gmail.com>
2024-11-16 19:11:57 -0300

    [main] style (HomeLayout/OurServicesSection)

--- src/layouts/HomeLayout/components/OurServicesSection/OurServicesSection.jsx
+++ src/layouts/HomeLayout/components/OurServicesSection/OurServicesSection.jsx
@@ -1,16 +1,38 @@
 import React from "react";
 
+import Image from "next/image";
+import dog from "public/icons/dog.svg";
+import track from "public/icons/dog-track.svg";
+import cat from "public/icons/cat.svg";
+import house from "public/icons/house.svg";
+
 import styles from "./styles.module.css";
+import { CARDS } from "./constants";
 
 import SubmitInput from "@/components/molecules/SubmitInput/SubmitInput";
 
 const OurServicesSection = ({ title, body, subtitle, handleLocation }) => {
+  const cards = CARDS({ dog, cat, house, track });
+
   return (
     <section className={styles["our-services-container"]}>
-      <h2 className={styles.title}>{title}</h2>
-      <p className={styles.body}>{body}</p>
-      <h3 className={styles.subtitle}>{subtitle}</h3>
-      <SubmitInput placeholder="Zip Code" onSubmit={handleLocation} />
+      <article className={styles["text-container"]}>
+        <h2 className={styles.title}>
+          {title || "Looking for information..."}
+        </h2>
+        <p className={styles.body}>{body}</p>
+        <h3 className={styles.subtitle}>{subtitle}</h3>
+        <SubmitInput placeholder="Zip Code" onSubmit={handleLocation} />
+      </article>
+
+      <div className={styles["cards-container"]}>
+        {cards.map(({ image, text }) => (
+          <div key={text} className={styles.card}>
+            <Image src={image} alt={text} className={styles["card-image"]} />
+            <p className={styles["text-image"]}>{text}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
---
+++ src/layouts/HomeLayout/components/OurServicesSection/constants.js
@@ -0,0 +1,19 @@
+export const CARDS = ({ dog, cat, house, track }) =>
+  Object.freeze([
+    {
+      image: dog,
+      text: "Dog Walking"
+    },
+    {
+      image: cat,
+      text: "Pet Sitting"
+    },
+    {
+      image: house,
+      text: "Overnight Car"
+    },
+    {
+      image: track,
+      text: "Other Services"
+    }
+  ]);
