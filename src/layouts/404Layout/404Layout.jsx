17110a689524c183404b89b53fc553f3e56d125d
Dariana Rengifo <darianarengifo@gmail.com>
2024-11-16 17:52:37 -0300

    [main] style (src/layouts/404Layout)

--- src/layouts/404Layout/404Layout.jsx
+++ src/layouts/404Layout/404Layout.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
-const NotFoundLayout = () => {
-  return <h1>Pagina no encontrada</h1>;
+import styles from "./styles.module.css";
+
+import Navbar from "@/components/organisms/NavBar/NavBar";
+import Button from "@/components/atoms/Button/Button";
+
+const NotFoundLayout = ({ navContent, handleReturn }) => {
+  return (
+    <section className={styles["layout-container"]}>
+      <Navbar {...navContent} />
+      <div className={styles["bg-container"]}>
+        <h2>Ooops! The page you are looking for does not exist.</h2>
+        <Button
+          style={{
+            margin: 0,
+            backgroundColor: "#ff6752"
+          }}
+          onClick={handleReturn}
+        >
+          RETURN TO HOME PAGE
+        </Button>
+      </div>
+    </section>
+  );
 };
 
 export default NotFoundLayout;
