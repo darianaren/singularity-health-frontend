86b53e24641e79b3e95a4d3d137f70f324fa87ab
Dariana Rengifo <darianarengifo@gmail.com>
2024-11-16 21:21:37 -0300

    [main] update (next.config.mjs)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["reqres.in"] // Agrega el dominio aquí
+  }
+};
 
 export default nextConfig;
